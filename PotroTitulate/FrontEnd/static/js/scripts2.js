9451ef89424ac6a4ab121dcda43ee42a20a09f75
EnriqueMAP <enrique.molina.aguilar@gmail.com>
2025-01-27 13:10:43 -0600

    Se actualizo el perfil de usuario y scripts2

--- PotroTitulate/FrontEnd/static/js/scripts2.js
+++ PotroTitulate/FrontEnd/static/js/scripts2.js
@@ -7,9 +7,87 @@ function showRequirements(option) {
     requisitosContainer.innerHTML = '';
 
     const requisitos = {
-        'Opción 1': ['Requisito A', 'Requisito B', 'Requisito C'],
-        'Opción 2': ['Requisito D', 'Requisito E'],
-        'Opción 3': ['Requisito F', 'Requisito G', 'Requisito H']
+        'Trabajo escrito': [
+            'Formato 8.1 con sus firmas',
+            'Aviso Firmado de Privacidad de la UAEM',
+            'Certificado de 100% de plan de estudios',
+            'Certificado de Servicio Social',
+            'Certificado de Prácticas Profesionales',
+            'Acta de nacimiento',
+            'Formato 8.3',
+            'Formato 8.5',
+            'Nuevo: Reporte de porcentaje de similitud',
+            'Formato 8.11',
+            'Certificado de Bachillerato',
+            'Constancia de no adeudo a biblioteca',
+            'Constancia de no adeudo a Dirección de Control Escolar',
+            'Constancia de no adeudo a la UAEM-Contraloría',
+            'Comprobante/Baucher de Expedición de Titulo',
+            'Comprobante-Baucher pago a derecho a Evaluación Profesional',
+            'Formato de Llenado de Datos Personales',
+            'Formato 8.9',
+            'Evaluación Profesional: Formato 8.2',
+            'Evaluación Profesional: Formato 8.4',
+            'Evaluación Profesional: Formato 8.6',
+            'Evaluación Profesional: Formato 8.8'      
+        ],
+
+        'Aprovechamiento académico': [
+            'Formato 8.1 con sus firmas',
+            'Certificado de 100% de plan de estudios',
+            'Certificado de Servicio Social',
+            'Certificado de Prácticas Profesionales',
+            'Acta de nacimiento',
+            'Aviso Firmado de Privacidad de la UAEM',
+            '*Oficio de solicitud',
+            'Constancia de no haber cometido Falta a la Comunidad Universitaria',
+            'Historial Académico/Trayectoria Académica',
+            'Certificado de Bachillerato',
+            'Constancia de no adeudo a biblioteca',
+            'Constancia de no adeudo a Dirección de Control Escolar',
+            'Constancia de no adeudo a la UAEM-Contraloría',
+            'Comprobante de Expedición de Titulo',
+            'Comprobante-Baucher pago a derecho a Evaluación Profesional',
+            'Formato de Llenado de Datos Personales',
+            'Formato 8.9'
+        ],
+
+        'Creditos en estudios avanzados': [
+            'Formato 8.1 con sus firmas',
+            'Certificado de 100% de plan de estudios',
+            'Certificado de Servicio Social',
+            'Certificado de Prácticas Profesionales',
+            'Acta de nacimiento',
+            'Certificado de Bachillerato',
+            'Constancia de no adeudo a biblioteca',
+            'Constancia de no adeudo a Dirección de Control Escolar',
+            'Constancia de no adeudo a la UAEM-Contraloría',
+            'Comprobante de Expedición de Titulo',
+            'Comprobante-Baucher pago a derecho a Evaluación Profesional',
+            'Aviso Firmado de Privacidad de la UAEM',
+            'Formato de Llenado de Datos Personales',
+            'Formato 8.9',
+            '*Oficio de solicitud',
+            'Certificado de Diplomado Superior'
+        ],
+        'Examen General para el egreso de la Licenciatura EGEL': [
+            'Formato 8.1 con sus firmas',
+            'Certificado de 100% de plan de estudios',
+            'Certificado de Servicio Social',
+            'Certificado de Prácticas Profesionales',
+            'Acta de nacimiento',
+            'Aviso firmado de privacidad de la UAEM',
+            'Oficio de solicitud',
+            'Certificado de bachillerato',
+            'Constancia de no adeudo a biblioteca',
+            'Constancia de no adeudo a Dirección de Control Escolar',
+            'Constancia de no adeudo a la UAEM-Contraloría',
+            'Comprobante de Expedición de Titulo',
+            'Comprobante-Baucher pago a derecho a Evaluación Profesional',
+            'Formato de Llenado de Datos Personales',
+            'Formato 8.9',
+            'Testimonio que les manda SENEVAL a su correo'
+        ]
     };
 
     const ul = document.createElement('ul');
